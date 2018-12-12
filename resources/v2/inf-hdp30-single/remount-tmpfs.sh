0e8d652ef1d8b2b2d4fbc99b8c43595829166b0a
Chaffelson <chaffelson@gmail.com>
2018-12-12 13:36:58 +0000

    fix tmp remount in inf-hdp30-single add inf-hdp31-single fix deploy.py to default to base Cloudbreak image when Stack version not recognised (for HDP-3.1 on older Cloudbreak instances)

--- resources/v2/inf-hdp30-single/remount-tmpfs.sh
+++ resources/v2/inf-hdp30-single/remount-tmpfs.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 #remount tmpfs to ensure NOEXEC is disabled
-mount -o remount,size=10G /tmp
-mount -o remount,exec /tmp
+if grep -Eq '^[^ ]+ /tmp [^ ]+ ([^ ]*,)?noexec[, ]' /proc/mounts; then
+  echo "/tmp found as noexec, remounting..."
+  mount -o remount,size=10G /tmp
+  mount -o remount,exec /tmp
+else
+  echo "/tmp not found as noexec, skipping..."
+fi
 
