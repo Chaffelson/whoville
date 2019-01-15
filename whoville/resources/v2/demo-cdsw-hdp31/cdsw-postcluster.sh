8e2875625bd9080bcb5f7d43be4b57428505b575
Chaffelson <chaffelson@gmail.com>
2019-01-15 14:29:48 -0700

    Typo

--- whoville/resources/v2/demo-cdsw-hdp31/cdsw-postcluster.sh
+++ whoville/resources/v2/demo-cdsw-hdp31/cdsw-postcluster.sh
@@ -68,7 +68,7 @@ chmod +x ./Anaconda2-5.2.0-Linux-x86_64.sh
 # CDSW Setup
 sed -i "s@MASTER_IP=\"\"@MASTER_IP=\"${MASTER_IP}\"@g" /etc/cdsw/config/cdsw.conf
 sed -i "s@JAVA_HOME=\"/usr/java/default\"@JAVA_HOME=\"$(echo ${JAVA_HOME})\"@g" /etc/cdsw/config/cdsw.conf
-sed -i "s@DOMAIN=\"cdsw.company.com\"@DOMAIN=\"${PUBLIC_IP}.nip.io\"@g" /etc/cdsw/config/cdsw.conf
+sed -i "s@DOMAIN=\"cdsw.company.com\"@DOMAIN=\"${MASTER_IP}.nip.io\"@g" /etc/cdsw/config/cdsw.conf
 sed -i "s@DOCKER_BLOCK_DEVICES=\"\"@DOCKER_BLOCK_DEVICES=\"${DOCKER_BLOCK}\"@g" /etc/cdsw/config/cdsw.conf
 sed -i "s@APPLICATION_BLOCK_DEVICE=\"\"@APPLICATION_BLOCK_DEVICE=\"${APP_BLOCK}\"@g" /etc/cdsw/config/cdsw.conf
 sed -i "s@DISTRO=\"\"@DISTRO=\"HDP\"@g" /etc/cdsw/config/cdsw.conf
