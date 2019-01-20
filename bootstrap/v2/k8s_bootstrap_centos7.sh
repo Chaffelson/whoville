be95f8f3e097584794a9b530bbe21eccba424b47
vvaks <vvaks@hw13930>
2019-01-20 10:45:16 -0500

    add start-k8s-service.sh & install docker-ce

--- bootstrap/v2/k8s_bootstrap_centos7.sh
+++ bootstrap/v2/k8s_bootstrap_centos7.sh
@@ -6,9 +6,9 @@ modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 swapoff -a
 
-yum install -y docker
-systemctl enable docker
-service docker start
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
 dhclient
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
@@ -25,5 +25,19 @@ EOF
 yum install -y kubelet kubeadm kubectl
 
 echo "complete" > /tmp/status.success
+
+cat <<EOF > /tmp/start-k8s-service.sh
+#!/bin/bash
+
+systemctl start docker
+systemctl enable docker
+systemctl start kubelet
+systemctl enable kubelet
+
+#sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+#systemctl daemon-reload
+#systemctl restart kubelet
+
+EOF
 
 reboot
