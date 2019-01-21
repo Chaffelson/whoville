d3f0bad74ae0febb747f790808141dc97c45f4c6
vvaks <vvaks@hw13930>
2019-01-20 21:05:24 -0500

    mod k8s_bootstrap

--- bootstrap/v2/k8s_bootstrap_centos7.sh
+++ bootstrap/v2/k8s_bootstrap_centos7.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash
 
 setenforce 0
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
@@ -6,10 +6,15 @@ modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 swapoff -a
 
-yum install -y docker
-systemctl enable docker
-service docker start
+echo "/dev/mapper/centos-swap swap swap defaults 0 0" >> /etc/fstab
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce
 dhclient
+
+systemctl start docker
+systemctl enable docker
 
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -24,20 +29,34 @@ EOF
 
 yum install -y kubelet kubeadm kubectl
 
-cat <<EOF > /tmp/start-k8s-service.sh
-#!/bin/bash
+echo "complete" > /tmp/status.success
 
-systemctl start docker
-systemctl enable docker
-systemctl start kubelet
-systemctl enable kubelet
+cat <<EOF > /tmp/prepare-k8s-service.sh
+#!/bin/bash
+#systemctl start kubelet
+#systemctl enable kubelet
 
 #sed -i 's/cgroup-driver=systemd/cgroup-driver=cgroupfs/g' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
 #systemctl daemon-reload
 #systemctl restart kubelet
 
-EOF
+sudo echo "KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs --runtime-cgroups=/systemd/system.slice --kubelet-cgroups=/systemd/system.slice" > /etc/sysconfig/kubelet
 
-echo "complete" > /tmp/status.success
+EOF
+chmod 755 /tmp/prepare-k8s-service.sh
+
+cat <<EOF > /tmp/initialize-k8s-cluster.sh
+#!/bin/bash
+
+#sudo kubeadm init --apiserver-advertise-address=$(ifconfig eth0|grep -Po 'inet [0-9.]+'|grep -Po '[0-9.]+') --pod-network-cidr=10.244.0.0/16 > /tmp/k8s-init.log
+
+mkdir -p ~/.kube
+sudo cp -if /etc/kubernetes/admin.conf ~/.kube/config
+sudo chown centos:centos ~/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+EOF
+chmod 755 /tmp/initialize-k8s-cluster.sh
 
 reboot
