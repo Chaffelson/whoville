1b0082572d2ebb00ee2445f6e6fa5fb6228d381d
Daniel Chaffelson <chaffelson@gmail.com>
2019-01-31 11:42:54 +0000

    Bump version: 0.2.0 → 0.3.0

--- whoville/__init__.py
+++ whoville/__init__.py
@@ -5,7 +5,7 @@ Whoville: An opinionated auto-deployer for the Hortonworks Platform
 """
 __author__ = """Daniel Chaffelson"""
 __email__ = 'chaffelson@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __all__ = ['config', 'deploy', 'utils', 'security', 'mayor', 'infra',
            'cloudbreak', 'actions', 'director']
 
