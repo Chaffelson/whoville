5e9d3a4a0c3b21cf52db907fed5f27032e9a4236
Daniel Chaffelson <chaffelson@gmail.com>
2019-07-30 13:54:07 +0100

    Upgrade CSDW to 1.6 Replace CDSW Python2 with Python3

--- whoville/resources/v2/demo-cdsw-hdp31/install-dependency-packages.sh
+++ whoville/resources/v2/demo-cdsw-hdp31/install-dependency-packages.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-yum install -y epel-release
-yum install -y python-pip
+# legacy Python2 install process, deprecated as numpy now requires py3.5+
+#yum install -y epel-release
+#yum install -y python-pip
+
+# Install python 3.5 for numpy 1.17+
+yum -yq install @development rh-python35
+source /opt/rh/rh-python35/enable
+export PYTHONHOME=/opt/rh/rh-python35/root/usr/
+export PYTHONPATH=/opt/rh/rh-python35/root/usr/lib64/python3.5
+pip install --upgrade pip
 pip install numpy
